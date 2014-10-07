49dff53218dbc0030dfc2fa0b659988c26a7534f
Chiarng Lin <chiarng@gmail.com>
2014-10-07 03:20:23 -0700

    css based fading from black

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,4 @@
 // JavaScript Document
 var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/";
 
-document.getElementById('bg').style.backgroundImage = 'url(https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/2014/10/2.JPG)';
+document.getElementById('bg').style.backgroundImage = 'url(https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/2014/10/1.JPG)';
