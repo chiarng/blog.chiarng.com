7b03c9eaf2529d6cbf03473233d9a58318d15efb
Chiarng Lin <chiarng@gmail.com>
2014-10-07 23:45:17 -0700

    load exif.js and imgSwap function

--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,10 @@
 // JavaScript Document
 var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/";
 
-document.getElementById('bg').style.backgroundImage = 'url(https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/2014/10/1.JPG)';
+var imgURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/2014/10/2.JPG";
+
+function imgSwap() {
+	document.getElementById('bg').style.backgroundImage = "url(" + imgURL + ")";
+	document.getElementById('leftblur').style.backgroundImage = "url(" + imgURL + ")";
+	document.getElementById('rightblur').style.backgroundImage = "url(" + imgURL + ")";
+}
