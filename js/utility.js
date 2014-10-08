1a2a7a410fc323cd53553e5d955f8bc2e3bb400b
Chiarng Lin <chiarng@gmail.com>
2014-10-08 04:57:22 -0700

    auto link creation and event handling loops

--- js/utility.js
+++ js/utility.js
@@ -1,16 +1,13 @@
 // JavaScript Document
-var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/";
-var imgURL= apiURL + "img/2014/10/1.JPG";
-var imgLink = document.getElementById('imgLink');
-//var indexlist= apiURL + "img/indexlist.txt"
 
+var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/";
+
+//swaps background image give imgURL
 function imgSwap(imgURL) {
 	document.getElementById('bg').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('leftblur').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('rightblur').style.backgroundImage = "url(" + imgURL + ")";
-}
-
-//function populateList(indexlist) {}
+};
 
 //cross-browser support for attaching event listeners
 function addEvent(element, event, func) {
@@ -18,7 +15,29 @@ function addEvent(element, event, func) {
 		return element.attachEvent('on' + event, func);
 	else
 		return element.addEventListener(event, func, false);
+};
+
+// loop through list of photo names
+// create hyperlink given date, title, imgURL, id
+// addEvent to the hyperlink given id and imgURL
+
+var imgList = [1,2,3];
+var x = '';
+
+for (i=1; i<imgList.length+1; i++) {
+	var x = '<a href id="imglink' + i + '"> Date and Title of Image </a> <br>' + x;
+};
+document.getElementById("imglist").innerHTML = x;
+
+for (ii=1; ii<imgList.length+1; ii++) {
+	(function (ii) {
+		addEvent (document.getElementById('imglink' + ii),'click',function(e) {
+			e.preventDefault();e.stopPropagation();imgSwap(apiURL + "img/2014/10/" + ii + ".JPG");
+			});
+	})(ii);
 }
 
-// addEvent (indexlist,'load',function(){populateList();})
+var imgURL = apiURL + "img/2014/10/1.JPG";
+var imgLink = document.getElementById('imglink');
+
 addEvent (imgLink,'click',function(e){e.preventDefault();e.stopPropagation();imgSwap(imgURL);});
