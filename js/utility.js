fb99d861be22b2b3c6dfb9b60dc7b92f9eba28e0
Chiarng Lin <chiarng@gmail.com>
2014-10-08 02:25:26 -0700

    OMFG it works! added event listener for link that changes background

--- js/utility.js
+++ js/utility.js
@@ -1,10 +1,24 @@
 // JavaScript Document
 var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/";
+var imgURL= apiURL + "img/2014/10/1.JPG";
+var imgLink = document.getElementById('imgLink');
+//var indexlist= apiURL + "img/indexlist.txt"
 
-var imgURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/2014/10/2.JPG";
-
-function imgSwap() {
+function imgSwap(imgURL) {
 	document.getElementById('bg').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('leftblur').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('rightblur').style.backgroundImage = "url(" + imgURL + ")";
 }
+
+//function populateList(indexlist) {}
+
+//cross-browser support for attaching event listeners
+function addEvent(element, event, func) {
+	if (element.attachEvent)
+		return element.attachEvent('on' + event, func);
+	else
+		return element.addEventListener(event, func, false);
+}
+
+// addEvent (indexlist,'load',function(){populateList();})
+addEvent (imgLink,'click',function(e){e.preventDefault();e.stopPropagation();imgSwap(imgURL);});
