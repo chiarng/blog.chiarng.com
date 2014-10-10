2fa6b41e36513338350835bd73f58c04394d4c4b
Chiarng Lin <chiarng@gmail.com>
2014-10-10 05:11:06 -0700

    finally have EXIF pulling working

--- js/utility.js
+++ js/utility.js
@@ -2,18 +2,41 @@
 
 // variable declarations
 var apiURL="https://s3-us-west-1.amazonaws.com/blog.chiarng.com/img/";
-var imgList = ["20141001","20141002","20141003"];
-var imgTitle = ["Lighthouse","Rose","Sunset"];
+var imgList = ["20131201","20131202","20131203","20140901","20140902","20141001","20141002","20141003"];
+var imgTitle = ["Girl1","Girl2","Girl3","Girl4","Girl5","Lighthouse","Rose","Sunset"];
 var imgLinks = '';
 
-//swaps background image give imgURL
+// swaps background image given imgURL
 function imgSwap(imgURL) {
 	document.getElementById('bg').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('leftblur').style.backgroundImage = "url(" + imgURL + ")";
 	document.getElementById('rightblur').style.backgroundImage = "url(" + imgURL + ")";
+	exifSwap(imgURL);
 };
 
-//cross-browser support for attaching event listeners
+// swaps EXIF given imgURL
+function exifSwap(imgURL) {
+	var invImg = document.createElement('img'); 
+	invImg.onload = function () {
+		EXIF.getData(invImg, function() {
+		document.getElementById("exifholder").innerHTML = 
+    		"Camera: " + EXIF.getTag(this,"Model") + "<br>" +
+    		"Date and Time: " + EXIF.getTag(this,"DateTimeOriginal") + "<br>" +
+    		"F-stop: f/" + EXIF.getTag(this,"FNumber") + "<br>" +
+    		"Exposure Time: 1/" + 1/EXIF.getTag(this,"ExposureTime") + " seconds <br>" +
+    		"ISO speed: " + EXIF.getTag(this,"ISOSpeedRatings") + "<br>" +
+    		"Focal Length: " + EXIF.getTag(this,"FocalLength") + "<br>" +
+    		"Exposure Program: " + EXIF.getTag(this,"ExposureProgram") + "<br>" +
+    		"Exposure Bias: " + EXIF.getTag(this,"ExposureBias") + "<br>" +
+    		"Metering Mode: " + EXIF.getTag(this,"MeteringMode") + "<br>" +
+    		"Flash: " + EXIF.getTag(this,"Flash") + "<br>" +
+    		"White Balance Mode: " + EXIF.getTag(this,"WhiteBalance");
+		});
+	};
+	invImg.src = imgURL;
+};
+
+// cross-browser support for attaching event listeners
 function addEvent(element, event, func) {
 	if (element.attachEvent)
 		return element.attachEvent('on' + event, func);
@@ -23,7 +46,7 @@ function addEvent(element, event, func) {
 
 function init() {
 	// load latest background image
-	imgSwap(apiURL + imgList[imgList.length-1].substring(0,4) + "/" + imgList[imgList.length-1].substring(4,6) + "/" + imgList[imgList.length-1].substring(6,9) + ".JPG");
+	imgSwap(apiURL + imgList[imgList.length-1].substring(0,4) + "/" + imgList[imgList.length-1].substring(4,6) + "/" + imgList[imgList.length-1].substring(6,9) + ".jpg");
 
 	// loop through list of photos and create hyperlink given date, title, imgURL, id
 	for (i=1; i<imgList.length+1; i++) {
@@ -37,8 +60,10 @@ function init() {
 	for (ii=1; ii<imgList.length+1; ii++) {
 		(function (ii) {
 			addEvent (document.getElementById('imglink' + ii),'click',function(e) {
-				e.preventDefault();e.stopPropagation();imgSwap(apiURL + imgList[ii-1].substring(0,4) + "/" + imgList[ii-1].substring(4,6) + "/" + imgList[ii-1].substring(6,9) + ".JPG");
-				});
+				e.preventDefault();
+				e.stopPropagation();
+				imgSwap(apiURL + imgList[ii-1].substring(0,4) + "/" + imgList[ii-1].substring(4,6) + "/" + imgList[ii-1].substring(6,9) + ".jpg");
+			});
 		})(ii);
 	};
 };
@@ -50,3 +75,6 @@ var readyStateCheckInterval = setInterval(function() {
         clearInterval(readyStateCheckInterval);
     }
 }, 10);
+
+
+	
